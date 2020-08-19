f2ec04e3ff517cc3a7ca80f857d63ceb1249fcdb
Chenkuan-GE <47959525+Chenkuan-GE@users.noreply.github.com>
2020-08-19 19:05:40 +1000

    Update A_star.py

--- A_star.py
+++ A_star.py
@@ -1,3 +1,10 @@
 from Queue import PriorityQueue
 
-class State
+class State(object):
+  def __init__(self,value,parent,start=0,goal=0,solver = 0):
+    self.children = []
+    self.parent = parent
+    self.value = value
+    self.dist = 0
+    if parent:
+       self.path = parent.path[:]
